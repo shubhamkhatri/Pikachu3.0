3a077206b92b6b14f79a967407f3c4a2e8a12361
shubhamkhatri <shubhamkhatri484@gmal.com>
2020-01-22 00:45:58 +0530

    updated again

--- app/src/main/java/com/example/android/pikachu/MainActivity.java
+++ app/src/main/java/com/example/android/pikachu/MainActivity.java
@@ -17,11 +17,15 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -29,16 +33,29 @@ import android.widget.Toast;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.FileOutputStream;
+import java.util.Locale;
+
+import com.itextpdf.text.Document;
+import java.text.SimpleDateFormat;
+
 
 public class MainActivity extends AppCompatActivity {
 
     EditText mResltEt;
     ImageView mPreviewIv;
+    Button mSaveBtn;
+
 
     public static final int CAMERA_REQUEST_CODE = 200;
     public static final int STORAGE_REQUEST_CODE = 400;
+    private static final int STORAGE_CODE = 600;
     public static final int IMAGE_PICK_GALLERY_CODE = 1000;
     public static final int IMAGE_PICK_CAMERA_CODE = 1001;
 
@@ -56,6 +73,32 @@ public class MainActivity extends AppCompatActivity {
 
         mResltEt = findViewById(R.id.resultEt);
         mPreviewIv = findViewById(R.id.imageIv);
+        mSaveBtn = findViewById(R.id.saveBtn);
+
+        mSaveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //we need to handle runtime permission for devices with marshmallow and above
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M){
+                    //system OS >= Marshmallow(6.0), check if permission is enabled or not
+                    if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                            PackageManager.PERMISSION_DENIED){
+                        //permission was not granted, request it
+                        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permissions, STORAGE_CODE);
+                    }
+                    else {
+                        //permission already granted, call save pdf method
+                        savePdf();
+                    }
+                }
+                else {
+                    //system OS < Marshmallow, call save pdf method
+                    savePdf();
+                }
+            }
+        });
+
         cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
         storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
     }
@@ -171,7 +214,7 @@ public class MainActivity extends AppCompatActivity {
 
                     boolean writeStorageAccepted = grantResults[0] ==
                             PackageManager.PERMISSION_GRANTED;
-                    if ( writeStorageAccepted) {
+                    if (writeStorageAccepted) {
                         pickGallery();
                     } else {
                         Toast.makeText(this, "permission denied", Toast.LENGTH_SHORT).show();
@@ -179,53 +222,98 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
 
+            case STORAGE_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission was granted from popup, call savepdf method
+                    savePdf();
+                } else {
+                    //permission was denied from popup, show error message
+                    Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+            break;
         }
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
-                CropImage.activity(data.getData())
-                        .setGuidelines(CropImageView.Guidelines.ON)
-                        .start(this);
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+            if (resultCode == RESULT_OK) {
+                if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                    CropImage.activity(data.getData())
+                            .setGuidelines(CropImageView.Guidelines.ON)
+                            .start(this);
+                }
+                if (requestCode == IMAGE_PICK_CAMERA_CODE) {
+                    CropImage.activity(image_uri)
+                            .setGuidelines(CropImageView.Guidelines.ON)
+                            .start(this);
+                }
             }
-            if (requestCode == IMAGE_PICK_CAMERA_CODE) {
-                CropImage.activity(image_uri)
-                        .setGuidelines(CropImageView.Guidelines.ON)
-                        .start(this);
+            if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+                if (resultCode == RESULT_OK) {
+                    Uri resultUri = result.getUri();
+                    mPreviewIv.setImageURI(resultUri);
+
+                    BitmapDrawable bitmapDrawable = (BitmapDrawable) mPreviewIv.getDrawable();
+                    Bitmap bitmap = bitmapDrawable.getBitmap();
+                    TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+                    if (!recognizer.isOperational()) {
+                        Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                        SparseArray<TextBlock> items = recognizer.detect(frame);
+                        StringBuilder sb = new StringBuilder();
+
+                        for (int i = 0; i < items.size(); i++) {
+                            TextBlock myItem = items.valueAt(i);
+                            sb.append(myItem.getValue());
+                            sb.append("\n");
+
+                        }
+                        mResltEt.setText(sb.toString());
+                    }
+                } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                    Exception error = result.getError();
+                    Toast.makeText(this, "" + error, Toast.LENGTH_SHORT).show();
+                }
             }
         }
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (resultCode == RESULT_OK) {
-                Uri resultUri = result.getUri();
-                mPreviewIv.setImageURI(resultUri);
 
-                BitmapDrawable bitmapDrawable = (BitmapDrawable) mPreviewIv.getDrawable();
-                Bitmap bitmap = bitmapDrawable.getBitmap();
-                TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
 
-                if (!recognizer.isOperational()) {
-                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
-                } else {
-                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-                    SparseArray<TextBlock> items = recognizer.detect(frame);
-                    StringBuilder sb = new StringBuilder();
+    private void savePdf() {
+        //create object of Document class
+        Document mDoc = new Document();
+        //pdf file name
+        String mFileName = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(System.currentTimeMillis());
+        //pdf file path
+        String mFilePath = Environment.getExternalStorageDirectory() + "/" + mFileName + ".pdf";
 
-                    for (int i = 0; i < items.size(); i++) {
-                        TextBlock myItem = items.valueAt(i);
-                        sb.append(myItem.getValue());
-                        sb.append("\n");
+        try {
+            //create instance of PdfWriter class
+            PdfWriter.getInstance(mDoc, new FileOutputStream(mFilePath));
+            //open the document for writing
+            mDoc.open();
+            //get text from EditText i.e. mTextEt
+            String mText = mResltEt.getText().toString();
 
-                    }
-                    mResltEt.setText(sb.toString());
-                }
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Exception error = result.getError();
-                Toast.makeText(this, "" + error, Toast.LENGTH_SHORT).show();
-            }
+            //add author of the document (optional)
+            mDoc.addAuthor("shubham");
+
+            //add paragraph to the document
+            mDoc.add(new Paragraph(mText));
+
+            //close the document
+            mDoc.close();
+            //show message that file is saved, it will show file name and file path too
+            Toast.makeText(this, mFileName +".pdf\nis saved to\n"+ mFilePath, Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e){
+            //if any thing goes wrong causing exception, get and show exception message
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 
